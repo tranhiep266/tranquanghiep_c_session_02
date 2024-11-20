dde6e65056512dfe2e6156b754b4a75249ee050d
tranhiep266 <hiep5apy@gmail.com>
2024-11-20 23:25:35 +0700

    Delete session2_ex1.cpp

--- session2_ex1.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-	int age = 25;          // Khai bao bien kieu so nguyên voi giá tri 25
-	float pi = 3.14;       // Khai bao bien kieu so thuc voi giá tri 3.14
-	char grade = 'A';      // Khai báo bien kieu ky tu voi giá tri 'A'
-	return 0;
-}
