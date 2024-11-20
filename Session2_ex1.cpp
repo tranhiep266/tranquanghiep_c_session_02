c5787fa3d0d0065432fdc1b2c450c36ffe1555c8
tranhiep266 <hiep5apy@gmail.com>
2024-11-20 23:25:20 +0700

    Add files via upload

---
+++ Session2_ex1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int age = 25;          // Khai bao bien kieu so nguyên voi giá tri 25
+	float height = 1.70;       // Khai bao bien kieu so thuc voi giá tri 3.14
+	char grade = 'A';      // Khai báo bien kieu ky tu voi giá tri 'A'
+	double pi = 3.141592653589793; // khai bao bien kieu du lieu so thuc chinh xac
+	return 0;
+}
